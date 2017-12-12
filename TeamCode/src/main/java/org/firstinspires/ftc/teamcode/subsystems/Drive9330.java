b871e956c9f796e2cb9ad5e127197a50f8632c2e
Lemon <liamshinx@gmail.com>
2017-12-11 16:46:28 -0800

    Editing to compensate for absolute gyro position

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Drive9330.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Drive9330.java
@@ -10,15 +10,16 @@ import org.firstinspires.ftc.teamcode.Hardware9330;
  */
 
 public class Drive9330 {
-    Double Diameter = 3.78;
-    Double Circumference = 3.1415 * Diameter;
+    double Diameter = 3.78;
+    double Circumference = 3.1415 * Diameter;
     int PPR = 560;
     int oneInchOfMovement = (int)Math.round(PPR / Circumference);
 
     private Hardware9330 hwMap = null;
     Telemetry telemetry = null;
-    Double encoderDriveSpeed = 0.4;
-    Integer turnError = 1 ;
+    double encoderDriveSpeed = 0.4;
+    int turnError = 1 ;
+    int straightError = 2;
     Gyro9330 gyro;
     int dist;
     int degree;
@@ -67,7 +68,6 @@ public class Drive9330 {
     }
 
     public void driveDistance(int inches, boolean useGyro) {
-        gyro.resetGyro();
         Hardware9330.rightMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         Hardware9330.leftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
@@ -80,12 +80,14 @@ public class Drive9330 {
         Hardware9330.rightMotor.setPower(encoderDriveSpeed);
         Hardware9330.leftMotor.setPower(encoderDriveSpeed);
 
+        double startPos = gyro.getYaw();
+
         while(Hardware9330.rightMotor.isBusy() || Hardware9330.leftMotor.isBusy()) {
             if(useGyro) {
-                if (gyro.getYaw() < 358 || gyro.getYaw() > 2) {
+                if (gyro.getYaw() < startPos - straightError|| gyro.getYaw() > startPos + straightError) {
                     Hardware9330.rightMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
                     Hardware9330.leftMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-                    gyroTurn(0, 0.2, true, false);
+                    gyroTurn((float) startPos, 0.2, true);
                     Hardware9330.rightMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
                     Hardware9330.leftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
                 }
@@ -98,19 +100,15 @@ public class Drive9330 {
         Hardware9330.leftMotor.setPower(0);
     }
 
-    public void gyroTurn(float degrees, double speed, boolean allWheel, boolean resetGyro) {
+    public void gyroTurn(float _degrees, double speed, boolean allWheel) {
        boolean isBackwards = false;
-       float Degrees = degrees;
-       if(degrees < 0) {
+       float degrees = _degrees;
+       if(_degrees < 0) {
            isBackwards = true;
-           Degrees = Math.abs(degrees);
+           degrees = Math.abs(_degrees);
        }
-       if (resetGyro)
-        gyro.resetGyro();
-       float minAngle = Degrees - turnError;
-       if (minAngle < 0) minAngle += 360;
-       float maxAngle = Degrees + turnError;
-       if (maxAngle > 360) maxAngle -= 360;
+       float minAngle = degrees - turnError + (float)gyro.getYaw();
+       float maxAngle = degrees + turnError + (float)gyro.getYaw();
         while (gyro.getYaw() < minAngle || gyro.getYaw() > maxAngle) {
             if (telemetry != null) {
                 telemetry.addData("Gyro angle", gyro.getYaw());
