1b513ebc64ac8543dcc24a4bbd90d4088927bb32
Jared Swislow <jayswis2002@gmail.com>
2018-09-24 16:42:25 -0700

    haha real drive base code probably --NOT JARED

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware9330.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware9330.java
@@ -1,8 +1,37 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
 /**
  * Created by robot on 9/24/2018.
  */
 
 public class Hardware9330 {
+
+    private HardwareMap hwMap = null;
+    public ElapsedTime runtime = new ElapsedTime();
+
+    public static DcMotor frontLeftMotor;
+    public static DcMotor frontRightMotor;
+    public static DcMotor rearLeftMotor;
+    public static DcMotor rearRightMotor;
+
+    public Hardware9330() {
+    }
+
+    public void init(HardwareMap hwMap) {
+        frontLeftMotor = hwMap.dcMotor.get("frontLeftMotor");
+        frontRightMotor = hwMap.dcMotor.get("frontRightMotor");
+        rearLeftMotor = hwMap.dcMotor.get("rearLeftMotor");
+        rearRightMotor = hwMap.dcMotor.get("rearRightMotor");
+
+        frontLeftMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+        frontRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        rearLeftMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+        rearRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+    }
+
 }
