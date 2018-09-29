96d046bbaf419b4c6b0db93a5fb1c4ed3fd1c846
micah MacDonald <micah3289@gmail.com>
2018-09-28 17:27:56 -0700

    Added some hardware map and drive changes
    Jared & Micah

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware9330.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware9330.java
@@ -1,4 +1,34 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
 public class Hardware9330 {
+    /* junha made me make this
+    */
+    public static DcMotor leftFront;
+    public static DcMotor leftBack;
+    public static DcMotor rightFront;
+    public static DcMotor rightBack;
+
+    HardwareMap hwMap = null;
+
+    public Hardware9330() {
+
+    }
+
+    public void init(HardwareMap ahwMap) {
+
+        hwMap = ahwMap;
+
+        leftFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        leftBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        leftFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        leftBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+    }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Drive9330.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Drive9330.java
@@ -1,4 +1,40 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
+import org.firstinspires.ftc.teamcode.Hardware9330;
+
 public class Drive9330 {
+    private Hardware9330 hwMap = null;
+
+    public Drive9330(Hardware9330 robotMap) {
+        hwMap = robotMap;
+    }
+
+    public void driveForward(double speed) { //Speed must be between -100 and 100
+        Hardware9330.leftFront.setPower(speed);
+        Hardware9330.leftBack.setPower(speed);
+        Hardware9330.rightFront.setPower(speed);
+        Hardware9330.rightBack.setPower(speed);
+    }
+
+    public void turnLeft(double speed) {
+        Hardware9330.leftFront.setPower(-speed);
+        Hardware9330.leftBack.setPower(-speed);
+        Hardware9330.rightFront.setPower(speed);
+        Hardware9330.rightBack.setPower(speed);
+    }
+
+    public void turnRight(double speed) {
+        Hardware9330.leftFront.setPower(speed);
+        Hardware9330.leftBack.setPower(speed);
+        Hardware9330.rightFront.setPower(-speed);
+        Hardware9330.rightBack.setPower(-speed);
+    }
+
+    public void stopDrive() {
+        Hardware9330.leftFront.setPower(0);
+        Hardware9330.leftBack.setPower(0);
+        Hardware9330.rightFront.setPower(0);
+        Hardware9330.rightBack.setPower(0);
+    }
+
 }
